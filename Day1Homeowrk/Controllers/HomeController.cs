6e6202ae0870c873c62e9bebb8328333757b9c7c
Hero-Liu <kip183@gmail.com>
2016-04-14 10:50:57 +0800

    implement add log repository

--- Day1Homeowrk/Controllers/HomeController.cs
+++ Day1Homeowrk/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Day1Homeowrk.Models;
+using Day1Homeowrk.Models.Repositories;
+using Day1Homeowrk.ViewModels;
 using System.Web.Mvc;
 
 namespace Day1Homeowrk.Controllers
@@ -11,6 +13,13 @@ namespace Day1Homeowrk.Controllers
         }
         public ActionResult ViewAndAddMoneyLog()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ViewAndAddMoneyLog(MoneyLog moneyLog)
+        {
+            MoneyLogRepositorie.AddMoneyLog(moneyLog);
+            var viewModel = new ViewAndAddMoneyLogViewModel { AddedLog=moneyLog, HistroyLog = MoneyLogRepositorie.GetAllMoneyLogs()};
             return View();
         }
         public ActionResult About()
---
+++ Day1Homeowrk/Models/Repositories/MoneyLogRepositorie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+namespace Day1Homeowrk.Models.Repositories
+{
+    public class MoneyLogRepositorie
+    {
+        private static List<MoneyLog> _Logs = new List<MoneyLog>();
+        public static void AddMoneyLog(MoneyLog log)
+        {
+            _Logs.Add(log);
+        }
+        public static List<MoneyLog> GetAllMoneyLogs()
+        {
+            return _Logs;
+        }
+    }
+}
