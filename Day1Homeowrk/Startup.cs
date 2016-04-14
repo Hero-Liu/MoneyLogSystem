5eed2e5a7e27c556442917520dd5568b4619aa80
Hero-Liu <kip183@gmail.com>
2016-04-14 08:19:44 +0800

    initial project

---
+++ Day1Homeowrk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Day1Homeowrk.Startup))]
+namespace Day1Homeowrk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
